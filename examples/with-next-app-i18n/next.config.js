7c73c18a665fe12968fe52f03e30bcd1526fd81c
Daniel Sinclair <4412473+DanielSinclair@users.noreply.github.com>
2025-12-21 23:00:59 -0500

    chore: next 19, react 19.2 and turbopack support (#2601)
    * chore: react 19.2.3 update
    
    * chore: upgrade vite to 7.3.0
    
    * chore: upgrade vanilla-extract packages
    
    * chore: claude commit instructions
    
    * chore: next-intl 4.6.1
    
    * chore: next 16.1.0, turbopack
    
    chore: revert styling next-env.d.ts
    
    chore: ignore next-env.d.ts formatting
    
    fix: lockfile
    
    fix: create-rainbowkit
    
    * chore: createNextIntlPlugin
    
    * fix: vanilla-extract webpack
    
    * chore: docs
    
    * chore: ignore next-env.d.ts updates

--- examples/with-next-app-i18n/next.config.js
+++ examples/with-next-app-i18n/next.config.js
@@ -1,14 +1,11 @@
 const createNextIntlPlugin = require('next-intl/plugin');
 
-const withNextIntl = createNextIntlPlugin();
+// Explicitly specify the i18n config path for Turbopack compatibility
+const withNextIntl = createNextIntlPlugin('./src/i18n/request.ts');
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  webpack: (config) => {
-    config.externals.push('pino-pretty', 'lokijs', 'encoding');
-    return config;
-  },
 };
 
 module.exports = withNextIntl(nextConfig);
